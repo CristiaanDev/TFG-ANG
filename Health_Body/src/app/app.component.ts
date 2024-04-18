a9a683bc2fd37cb09fa11e3690154c54a4db9480
Cristian <cristiaanies@gmail.com>
2024-04-18 23:21:49 +0200

    nav

--- Health_Body/src/app/app.component.ts
+++ Health_Body/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { CuentaComponent } from './components/cuenta/cuenta.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InicioComponent],
+  imports: [RouterOutlet, InicioComponent, CuentaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- Health_Body/src/app/components/cuenta/cuenta.component.ts
+++ Health_Body/src/app/components/cuenta/cuenta.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-cuenta',
   standalone: true,
-  imports: [],
+  imports: [MatSidenavModule],
   templateUrl: './cuenta.component.html',
   styleUrl: './cuenta.component.scss'
 })
 export class CuentaComponent {
-
 }
