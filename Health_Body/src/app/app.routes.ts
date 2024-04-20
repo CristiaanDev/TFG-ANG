2589cc0b02bacee5aeb940beca6927f04854a650
Cristian <cristiaanies@gmail.com>
2024-04-20 13:01:58 +0200

    pff

--- Health_Body/src/app/app.routes.ts
+++ Health_Body/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'Inicio',
+    loadComponent: () => import('./components/inicio/inicio.component').then((m) => m.InicioComponent),
+  },
+  {
+    path: '',
+    redirectTo: 'Inicio',
+    pathMatch: 'full',
+  },
+  {
+    path: 'cuenta',
+    loadComponent: () => import('./components/cuenta/cuenta.component').then((m) => m.CuentaComponent),
+  },
+
+];
--- Health_Body/src/app/components/cuenta/cuenta.component.ts
+++ Health_Body/src/app/components/cuenta/cuenta.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import { RouterLink } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
 
 @Component({
   selector: 'app-cuenta',
   standalone: true,
-  imports: [MatSidenavModule],
+  imports: [MatSidenavModule, RouterLink,MatButtonModule, MatCardModule, MatInputModule, MatFormFieldModule, FormsModule,IconFieldModule, InputIconModule],
   templateUrl: './cuenta.component.html',
   styleUrl: './cuenta.component.scss'
 })
