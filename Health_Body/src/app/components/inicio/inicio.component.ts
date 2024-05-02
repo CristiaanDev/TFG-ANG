6fa67fca14ad980865ad6af762fd0a68d5f4925f
CristiaanDev <cristiaan.dev@gmail.com>
2024-05-02 19:17:37 +0200

    pcc

--- Health_Body/src/app/components/inicio/inicio.component.ts
+++ Health_Body/src/app/components/inicio/inicio.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { AvisoComponent } from "../aviso/aviso.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
     selector: 'app-inicio',
     standalone: true,
     templateUrl: './inicio.component.html',
     styleUrl: './inicio.component.scss',
-    imports: [HeaderComponent, AvisoComponent]
+    imports: [HeaderComponent, AvisoComponent, FooterComponent]
 })
 export class InicioComponent {
 
