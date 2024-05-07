d88693dc25567491c710618b3bc09b466198c59d
CristiaanDev <cristiaan.dev@gmail.com>
2024-05-07 15:34:23 +0200

    inicio y registro casi hechos, blog a medias

--- Health_Body/src/app/components/login/login.component.ts
+++ Health_Body/src/app/components/login/login.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { AvisoComponent } from '../aviso/aviso.component';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
-  imports: [AvisoComponent, HeaderComponent, FooterComponent],
+  imports: [AvisoComponent, HeaderComponent, FooterComponent, RouterLink],
 })
 export class LoginComponent {}
--- Health_Body/src/app/components/registro/registro.component.ts
+++ Health_Body/src/app/components/registro/registro.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../footer/footer.component";
+import { HeaderComponent } from "../header/header.component";
+import { AvisoComponent } from "../aviso/aviso.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-registro',
-  standalone: true,
-  imports: [],
-  templateUrl: './registro.component.html',
-  styleUrl: './registro.component.scss'
+    selector: 'app-registro',
+    standalone: true,
+    templateUrl: './registro.component.html',
+    styleUrl: './registro.component.scss',
+    imports: [FooterComponent, HeaderComponent, AvisoComponent, RouterLink]
 })
 export class RegistroComponent {
 
