40cfe4737ae2e916b3cff6afdbfebb423261b146
Cristian <cristiaanies@gmail.com>
2024-05-07 23:03:19 +0200

    .

--- Health_Body/src/app/services/auth.service.ts
+++ Health_Body/src/app/services/auth.service.ts
@@ -1,9 +1,33 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
+import {
+  createUserWithEmailAndPassword,
+  updateCurrentUser,
+  updateProfile,
+} from 'firebase/auth';
+import { Observable, from } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  firebaseAuth = inject(Auth);
 
-  constructor() { }
+  registro(
+    email: string,
+    username: string,
+    password: string
+  ): Observable<void> {
+    const promise = createUserWithEmailAndPassword(
+      this.firebaseAuth,
+      email,
+      password
+    ).then((response) =>
+      updateProfile(response.user, { displayName: username })
+    );
+
+    return from(promise);
+  }
+
+  constructor() {}
 }
